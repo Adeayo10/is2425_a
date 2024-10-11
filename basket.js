97f4b71b867be8a864658decc96c4a92f632b8ba
Adeayo <aadelee74@gmail.com>
2024-10-11 13:23:59 +0100

    Refactor basket.js to add a function for calculating the total cost of a basket

--- basket.js
+++ basket.js
@@ -1,14 +1,18 @@
-// create a function that takes two objects, basket and prices as parameteres where basket is a mapping from products
-// to quantities and prices is a mapping from products to prices. The function should return the total cost of the basket
-// as a number.
+/*
+Create a function that takes two objects,
+basket and prices as parameters
+where basket is a mapping from products (string) to quantities (number)
+and prices is a mapping from products(String) to prices (number)
+and returns the total cost of the basket
+*/
 
+basket = { apple: 2, banana: 3, cherry: 5 };
+prices = { apple: 0.5, banana: 0.8, cherry: 0.2 };
 
-function calculateTotalCost(basket, prices) {
-    let totalCost = 0;
-    for (const product in basket) {
-        if (basket.hasOwnProperty(product) && prices.hasOwnProperty(product)) {
-            totalCost += basket[product] * prices[product];
-        }
-    }
-    return totalCost;
+function totalBasketCost(basket, prices) {
+  let total = 0;
+  for (let product in basket) {
+    total += basket[product] * prices[product];
+  }
+  return total;
 }
